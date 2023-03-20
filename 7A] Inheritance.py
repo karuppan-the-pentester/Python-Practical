08e9ade9ba6da8af95f87270ef2b33e4c886c254
Karuppasamy K <75080347+karuppan-the-pentester@users.noreply.github.com>
2023-03-20 03:17:04 -0700

    Create 7A] Inheritance.py

---
+++ 7A] Inheritance.py
@@ -0,0 +1,33 @@
+class Person(object):
+ def get_name(self,name):
+ self.name = name
+ def get_details(self):
+ return self.name
+class Student(Person):
+ def fill_details(self,name,branch,year):
+ self.name = name
+ self.branch = branch
+ self.year = year
+ def get_details(self):
+Ex.No: IMPLEMENTATION OF PYTHON PROGRAM FOR
+INHERITANCE AND METHOD OVERRIDING
+Date:
+ print("Name: ",self.name)
+ print("Branch: ",self.branch)
+ print("Year: ",self.year)
+class Teacher(Person):
+ def fill_details(self,name,branch):
+ self.name = name
+ self.branch = branch
+ def get_details(self):
+ print("Name: ",self.name)
+ print("Branch: ",self.branch)
+person1=Person()
+student1=Student()
+teacher1=Teacher()
+person1.get_name("Lakshmi")
+student1.fill_details("Kannan","ECE",2020)
+teacher1.fill_details("Meena","ECE")
+print(person1.get_details())
+print(student1.get_details())
+print(teacher1.get_details())
