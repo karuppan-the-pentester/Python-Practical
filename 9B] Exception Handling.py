4f0a8e76d23e48f1e3ff317f8356c0fc3eb5b942
Karuppasamy K <75080347+karuppan-the-pentester@users.noreply.github.com>
2023-03-20 03:20:52 -0700

    Create 9B] Exception Handling.py

---
+++ 9B] Exception Handling.py
@@ -0,0 +1,10 @@
+while True:
+ try:
+ a = int(input("Enter an integer: "))
+ div = 10/a
+ break
+ except:
+ print("Error Occured")
+ print("Please Enter Valid Value")
+ print()
+print ("Division is",div)
