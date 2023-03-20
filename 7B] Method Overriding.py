e89aa62edf80ceb353f6a3d3edc0d6358b1b3aea
Karuppasamy K <75080347+karuppan-the-pentester@users.noreply.github.com>
2023-03-20 03:17:52 -0700

    Create 7B] Method Overriding.py

---
+++ 7B] Method Overriding.py
@@ -0,0 +1,10 @@
+class Parent:
+def ovr_method(self):
+ print ("This is in Parent Class")
+class Child(Parent):
+def ovr_method(self):
+ print ("This is in Child Class")
+a=Parent()
+a.ovr_method()
+c = Child()
+c.ovr_method()
