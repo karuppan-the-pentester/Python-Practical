deda650c5d087b134f3ef11871e9623d1c7399fb
Karuppasamy K <75080347+karuppan-the-pentester@users.noreply.github.com>
2023-03-20 03:09:17 -0700

    Create 3B] User-Defined Function.py

---
+++ 3B] User-Defined Function.py
@@ -0,0 +1,12 @@
+def sum(a,b):
+ add=a+b
+ return(add)
+
+r=sum(16,33)
+print(r)
+
+def print_lines():
+ print ("Hello Python!")
+ print ("Welcome to Python Programming!!")
+  
+print(print_lines())
